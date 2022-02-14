1c285af6fe8d0f93b09643e6191e4800f9db26e8
Fernando Prado <feprado@microsoft.com>
2022-02-14 16:18:03 -0500

    The shopping cart now shows items

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,12 @@ import { useState } from "react";
 import { useQuery } from "react-query";
 //Components 
 import Item from './Item/Item';
+import Cart from './Cart/Cart';
 import Drawer from '@material-ui/core/Drawer';
 import LinearProgress  from "@material-ui/core/LinearProgress";
 import Grid from "@material-ui/core/Grid";
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
-import { Badge } from "@material-ui/core";
+import Badge from "@material-ui/core/Badge";
 
 //Styles
 import { Wrapper, StyledButton} from './App.Styles';
@@ -35,11 +36,38 @@ const App = () => {
     );
   console.log(data); 
 
-  const getTotalItems = () => null;
+  const getTotalItems = (items: CartItemType[]) => 
+    items.reduce((ack: number, item) => ack + item.amount, 0);
 
-  const handleAddToCart =(clickedItem: CartItemType) => null;
+  const handleAddToCart =(clickedItem: CartItemType) => {
+    setCartItems(prev => {
+      // 1. Is the item already added in the cart?
+      const isItemInCart = prev.find(item => item.id === clickedItem.id)
 
-  const handleRemoveFromCart = () => null;
+      if (isItemInCart) {
+        return prev.map(item => 
+          item.id === clickedItem.id
+          ? {...item, amount: item.amount + 1 }
+          : item
+        );
+      }
+      //First time the item is added
+      return[...prev, {...clickedItem, amount: 1}];
+    });
+  };
+
+  const handleRemoveFromCart = (id: number) => {
+    setCartItems(prev => 
+      prev.reduce((ack,item) => {
+        if (item.id === id) {
+          if (item.amount === 1) return ack;
+          return [...ack, {...item, amount: item.amount - 1}];
+        } else {
+          return [...ack, item];
+        }
+      }, [] as CartItemType[])
+    );
+  };
 
   if (isLoading) return <LinearProgress />;
   if (error) return <div> Something went wrong... </div>;
@@ -49,10 +77,16 @@ const App = () => {
   return (
     <Wrapper>
       <Drawer anchor='right' open={cartOpen} onClose = {() => setCartOpen(false)}>
-        Cart goes here
+        <Cart 
+          cartItems = {cartItems} 
+          addToCart = {handleAddToCart}
+          removeFromCart = {handleRemoveFromCart}
+          />
       </Drawer>
       <StyledButton onClick = {() => setCartOpen(true)}>
-
+        <Badge badgeContent={getTotalItems(cartItems)} color='error'>
+          <AddShoppingCartIcon/>
+        </Badge>
       </StyledButton>
       <Grid container spacing={3}>
       {data?.map(item => (
