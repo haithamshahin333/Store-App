50c11142cec940aab648fb11e3ffa76b967679f3
Haitham Shahin <42976403+haithamshahin333@users.noreply.github.com>
2022-02-15 13:20:32 -0500

    updated

--- src/AppInsights.js
+++ src/AppInsights.js
@@ -9,6 +9,8 @@ const appInsights = new ApplicationInsights({
     config: {
         connectionString: 'InstrumentationKey=a983f28b-b10e-4a57-a113-d5edfede053e;IngestionEndpoint=https://eastus-8.in.applicationinsights.azure.com/',
         extensions: [reactPlugin],
+        enableAutoRouteTracking: true,
+        disableFetchTracking: false,
         extensionConfig: {
           [reactPlugin.identifier]: { history: browserHistory }
         }
@@ -23,6 +25,5 @@ var telemetryInitializer = (envelope) => {
   envelope.tags["ai.cloud.roleInstance"] = "FrontEnd_StoreApp_V1";
 }
 appInsights.addTelemetryInitializer(telemetryInitializer);
-
 
 export { reactPlugin, appInsights };
